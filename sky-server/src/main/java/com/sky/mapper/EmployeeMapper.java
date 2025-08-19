10dd9f8cedf966f4eaf41dcf9a75b01936bae646
melody <1097379599@qq.com>
2025-08-19 18:57:20 +0800

    编辑员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -31,4 +31,7 @@ public interface EmployeeMapper {
      * @param employee
      */
     void update(Employee employee);
+
+    @Select("select * from employee where id=#{id}")
+    Employee getById(Long id);
 }
